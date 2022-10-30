4320f21ce9e56279217409f34aea8f0fd496273f
Lonnie Johnson <lonniejohnson@rocketmortgage.com>
2022-10-30 16:29:45 -0400

    2nd Assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]', //You can place the selector in brackets like we did here and use it as an CSS element as well 
-  template: `<app-server></app-server>
-    <p>testing this here</p>
-  <app-server></app-server>`,
+  selector: 'app-servers', //You can place the selector in brackets like we did here and use it as an CSS element as well 
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false; 
+  serverCreationStatus = 'No server was created'; 
+  serverName = 'TestServer';
+  userName = ''; 
+  //The method in the constructor doesn't have to be called because it's in the constructor lol
+  
+  constructor() {
+
+    setTimeout(() => {
+      this.allowNewServer = true; 
+    }, 2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+      this.serverCreationStatus = 'Server was created! Name is ' + this.serverName; 
+  }
+
+  //This is explicit casting using the HTML Input Element
+  //Event is an interface thats created on the DOM 
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value; 
+  }
+
+  onEmptyString(){
+    this.userName = ''; 
   }
 
 }
